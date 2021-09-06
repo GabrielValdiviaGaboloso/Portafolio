b8d3141d9dbbd38695395703015b79bd1406ca45
GabrielValdiviaGaboloso <g.valdivia1436@hotmail.com>
2021-09-06 19:42:47 -0300

    first commit

---
+++ src/Componente/cuerpo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function cuerpo() {
+    return (
+        <div className="cuerpo">
+            <div className="row">
+                <div className="col-12">
+                    <div className="Hola Mundo"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
